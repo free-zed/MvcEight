a9b221201b3aeb2decf039e5c447f0b573f59f8c
Afsana Tasnim <afsanatasnim780@yahoo.com>
2016-11-08 20:27:35 +0600

    UI Works

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcEight.Startup))]
+namespace MvcEight
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
